90fa9fc3e17e51e3faafecce455e0e6d1166b3b0
WangZihao <2597160811@qq.com>
2022-07-11 22:54:29 +0800

    feat(request.js): 请求头发送token

--- src/api/user.js
+++ src/api/user.js
@@ -2,10 +2,14 @@
  * @Author: WangZihao 2597160811@qq.com
  * @Date: 2022-07-11 22:07:13
  * @LastEditors: WangZihao 2597160811@qq.com
- * @LastEditTime: 2022-07-11 22:08:51
+ * @LastEditTime: 2022-07-11 22:14:10
  * @FilePath: \meyerweb\src\api\user.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
+import request from '@/utils/request'
 /**
- * 
+ * 新增用户
  */
+export const addUser = (data = {}) => {
+  return request({ url: '/sys/user/add', method: 'POST', data })
+}
--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@
  * @Author: WangZihao 2597160811@qq.com
  * @Date: 2022-07-11 15:59:17
  * @LastEditors: WangZihao 2597160811@qq.com
- * @LastEditTime: 2022-07-11 21:59:03
+ * @LastEditTime: 2022-07-11 22:53:36
  * @FilePath: \meyerweb\src\utils\request.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -12,15 +12,22 @@ import axios from 'axios'
 import { Message } from 'element-ui'
 // 引入枚举消息值
 import enumerationObj from '../enumeration/request-message'
+// 引入store实例
+import store from '../store/getters'
 
+// 创建axios实例
 const instance = axios.create({
   baseURL: process.env.VUE_APP_BASE_API,
   timeout: 10000,
   headers: { 'X-Custom-Header': 'foobar' }
 })
 
+// 请求拦截器
 instance.interceptors.request.use(
   function (config) {
+    // console.log(store.token)
+    const token = store.token
+    if (token) config.headers.Authorization = token
     return config
   },
   function (error) {
@@ -28,6 +35,7 @@ instance.interceptors.request.use(
   }
 )
 
+// 响应拦截器
 instance.interceptors.response.use(
   function (response) {
     console.log(response)
@@ -60,4 +68,5 @@ function request(options) {
   return instance(options)
 }
 
+// 导出axios实例
 export default request
